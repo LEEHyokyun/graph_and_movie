fe3f3e91104ba1eda473fa83bb917b69bcc67b3c
hyokyun <gyrbs22@gmail.com>
2021-08-26 12:56:09 +0900

    get data by using useQuery(Apollo hooks) and GraphQL Query made by myself

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import {gql, useQuery} from '@apollo/client'
+
+const getmovie = gql`
+{
+    getmovie{
+        id
+        medium_cover_image
+      }
+}
+`
 
 function Home() {
+    //const res = useQuery(getmovie)
+    const {loading, error, data} = useQuery(getmovie)
+    console.log(loading, error, data)
     return(
         <h2>HOME</h2>
     )
